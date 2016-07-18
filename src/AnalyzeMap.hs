27700542e4bc7625273c480dfd79452620154b2d
Alli Nilles <anilles137@gmail.com>
2016-07-18 16:30:53 -0500

    can now make cobweb plots

--- src/AnalyzeMap.hs
+++ src/AnalyzeMap.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances           #-}
+
 module AnalyzeMap
     (
       mkChart
@@ -9,7 +10,7 @@ import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Diagrams
 import Diagrams.Prelude
 import Control.Monad
-import BounceSim (BounceState(..), Poly, pts2path, nextBounce)
+import BounceSim (BounceState(..), Poly, pts2poly, nextBounce)
 import Maps
 
 log_map :: Double -> Double -> Double
@@ -26,24 +27,42 @@ scanPerimeter f =
     in      [ (x, f x) | x <- xs ]
 
 mkStar :: Poly Double
-mkStar = pts2path Maps.star
+mkStar = pts2poly Maps.star
 
 mkScan :: Poly Double -> Angle Double -> [(Double, Double)]
 mkScan poly ang = scanPerimeter (bounce_map poly ang)
 
-mkRecurr :: Poly Double -> Angle Double -> [(Double, Double)]
-mkRecurr poly ang =
-    let bounceSequence = iterate (bounce_map poly ang) 0.1
-    in  take 100 $ zip bounceSequence (tail bounceSequence)
+-- generates infinite list
+-- see https://en.wikipedia.org/wiki/Poincaré_plot
+mkReturnMap :: (Double -> Double) -> Double -> [(Double, Double)]
+mkReturnMap f x0 =
+    let xs = iterate f x0
+    in  zip xs (tail xs)
+
+-- generates infinite list
+-- see https://en.wikipedia.org/wiki/Cobweb_plot
+mkCobweb :: Poly Double -> Angle Double -> Double -> [(Double,Double)]
+mkCobweb poly ang s0 =
+    let bmap = bounce_map poly ang
+        fx0 = bmap s0
+        ffx0 = bmap fx0
+    in  (s0, fx0):(fx0,fx0):(fx0,ffx0):(ffx0,ffx0):(mkCobweb poly ang ffx0)
 
 -- given a polygon and list of bounce angles to try (ie: [pi/4, pi/2, 3*pi/4])
 -- writes resulting recurrence plot in svg to file
-mkChart :: Poly Double -> [Double] -> String -> IO ()
-mkChart poly queryAngs chartType =
+mkChart :: Poly Double -> [Double] -> Int -> String -> String -> IO ()
+mkChart poly queryAngs num chartType fname =
     let angles = map (@@ rad) queryAngs
+        start = 0.3
+        bFn a = bounce_map poly a
         chartF = case chartType of
-                    "scan" -> (\_ a -> plot $ points (show a) $ mkScan poly a)
-                    "recurr" -> (\_ a -> plot $ line (show a) $ [mkRecurr poly a])
-    in  toFile def "one_step.svg" $ do
+                    "scan"      ->  (\_ a -> plot $ points (show a) $
+                                    mkScan poly a)
+                    "recurr"    ->  (\_ a -> plot $ line (show a) $
+                                    [take num $ mkReturnMap (bFn a) start])
+                    "cobweb"    ->  (\_ a -> plot $ line (show a) $
+                                    [take num $ mkCobweb poly a start])
+                    _           ->  error "chart type not defined"
+    in  toFile def fname $ do
         foldM chartF () angles
         plot $ line "y=x" [scanPerimeter id]
