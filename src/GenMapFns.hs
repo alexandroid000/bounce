e153a1f9a32bff00498f3b282f1a413751d26ec0
Alli Nilles <anilles137@gmail.com>
2017-10-31 16:57:10 -0500

    GenMapFns: polyLenAngs seems to work. Need to write tests

--- src/GenMapFns.hs
+++ src/GenMapFns.hs
@@ -10,16 +10,23 @@ polyOffsets :: Poly V2 Double -> [V2 Double]
 polyOffsets p = trailOffsets $ unLoc p
 
 polyLens :: Poly V2 Double -> [Double]
-polyLens p = let
-    offs = polyOffsets p
-    norm v = sqrt (v `dot` v)
-    in map norm offs
+polyLens = map (\v -> sqrt (v `dot` v)) . polyOffsets
+
+-- rotate so v1 is along x axis
+-- then two cases for reflex and non-reflex angles
+angleInPoly :: V2 Double -> V2 Double -> Angle Double
+angleInPoly v1' v2' = let
+    th = (0 @@ rad) ^-^ (v1' ^. _theta)
+    v1 = rotate th v1'
+    v2 = rotate th v2'
+    phi = v2 ^. _theta
+    in (pi @@ rad) ^-^ phi
 
 polyAngs :: Poly V2 Double -> [Angle Double]
 polyAngs p = let
     offs = polyOffsets p
     offset_pairs = (zip offs (tail offs)) ++ [(last offs, head offs)]
-    get_ang (v1, v2) = angleBetween v1 v2
+    get_ang (v1, v2) = angleInPoly v1 v2
     in map get_ang offset_pairs
 
 polyLenAngs :: Poly V2 Double -> [(Double, Angle Double)]
