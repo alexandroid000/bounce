acbbb076afe26e14daba145afe6423efe2412c23
Alli Nilles <anilles137@gmail.com>
2017-11-03 15:45:52 -0500

    GenMapFns: first pass at working fixed point calcs

--- src/GenMapFns.hs
+++ src/GenMapFns.hs
@@ -31,3 +31,41 @@ polyAngs p = let
 
 polyLenAngs :: Poly V2 Double -> [(Double, Angle Double)]
 polyLenAngs p = zip (polyLens p) (polyAngs p)
+
+-- Calculating fixed point for sequential edge bouncing in convex polygons
+-- seems to have a sign error for even-sided polygons
+-- need to test with non-regular polygons and get plotting working
+
+coeff :: Angle Double -> Angle Double -> Double
+coeff theta phi = (cosA theta)/(cosA (theta ^-^ phi))
+
+coeff_prod :: Angle Double -> [(Double, Angle Double)] -> Double
+coeff_prod theta lenangs = let
+    coeffs = map (\(l,p) -> coeff theta p) lenangs
+    in foldr (*) 1.0 coeffs
+
+xfpNumerator :: Int -> Int -> Angle Double -> [(Double, Angle Double)] -> Double
+xfpNumerator (-1) _ _ _ = 0.0
+xfpNumerator i n theta la@((li, phi):lenangs) = let
+    sign_l = li*(-1)^(n-1-i)
+    in sign_l*(coeff_prod theta la) + (xfpNumerator (i-1) n theta lenangs)
+
+xfpDenom n theta lenangs = let
+    in 1 - (coeff_prod theta lenangs)*(-1)^n
+
+xfp :: Poly V2 Double -> Angle Double -> Double
+xfp poly theta = let
+    lenangs = polyLenAngs poly
+    n = length lenangs
+    in (xfpNumerator (n-1) n theta lenangs)/(xfpDenom n theta lenangs)
+
+
+phi = 5*pi/7 @@ rad
+th = 1.2 @@ rad
+c = coeff th phi
+xfp_correct = 500.0*c/(1+c)
+
+test = do
+    print $ xfp (mkPoly hep) (1.2 @@ rad)
+    print $ xfp_correct
+
