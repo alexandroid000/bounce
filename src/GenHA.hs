9bdeb3a5399865580305c76b02846cd3afede92c
Alexandra Nilles <anilles137@gmail.com>
2017-11-30 10:22:28 -0600

    know how to do bounce efficiently

--- src/GenHA.hs
+++ src/GenHA.hs
@@ -5,10 +5,20 @@ import Diagrams.Prelude
 import BounceSim
 import Maps
 
-type Segment = (P2 Double, P2 Double)
+type Seg = (P2 Double, P2 Double)
 
 loc1 = Location 1 "interior" "" "x'=vx &amp; y'=vy"
 t1 = Transition 1 1 "e1" "guard" "assignment"
+
+-- theta_e: angle between edge e and global x-axis
+-- theta_c: control input (angle to leave edge e at)
+-- vx = cos(theta)
+-- vy = sin(theta)
+-- vx^2 + vy^2 = 1 (invariant, normalize / check for floating point err)
+-- updates: theta := theta_e + theta_c
+-- cos(theta) := cos(theta_e)cos(theta_c)-sin(theta_e)sin(theta_c)
+-- sin(theta) := sin(theta_e)cos(theta_c)+cos(theta_e)sin(theta_c)
+
 
 --mk_HA :: Poly V2 Double -> Angle Double -> HA
 mk_pairs poly = let
@@ -18,7 +28,7 @@ mk_pairs poly = let
     mkpairs (a:b:ls) = (a,b):(mkpairs (b:ls))
     in mkpairs pts
 
-is_on_edge :: Segment -> String
+is_on_edge :: Seg -> String
 is_on_edge (pt1, pt2) =
     let eps = 0.001
         (x1,y1) = unp2 pt1
@@ -27,7 +37,7 @@ is_on_edge (pt1, pt2) =
         s2 = "(y-("++(show y1)++"))/("++(show y2)++" - ("++(show y1)++"))"
     in "|"++s1++"-"++s2++"| "++"&lt; "++(show eps) ++" && "++"(0.0 &lt;= "++s1++") && ("++s1++"&lt; 1.0)"
 
-mkBounceTran :: String -> Segment -> Transition
+mkBounceTran :: String -> Seg -> Transition
 mkBounceTran label (pt1, pt2) =
     let guard = is_on_edge (pt1, pt2)
         assign = "assign"
@@ -41,14 +51,19 @@ mkBounceTrans poly = let
     in map (uncurry mkBounceTran) (zip labels edges)
 
 
--- TODO
---rotate_relative_to_edge :: Angle Double -> Angle Double -> Segment -> Angle Double
---rotate_relative_to_edge alpha theta (pt1, pt2) = let
---    edge_vect = pt2 .-. pt1
---    theta' = (pi/2 -(theta ^. rad)) @@ rad
---    bounce_vect = edge_vect # rotate theta'
-    
+-- rotates incoming angle to new one after bounce
+-- angles \in [-pi,pi], relative to x axis
+rotate_relative_to_edge :: Angle Double -> Angle Double -> Seg -> Angle Double
+rotate_relative_to_edge alpha theta (pt1, pt2) = let
+    edge_vect = pt2 .-. pt1
+    theta' = (pi/2 -(theta ^. rad)) @@ rad
+    bounce_vect = edge_vect # rotate theta'
+    in signedAngleBetween bounce_vect unitX
 
+
+mkAssign :: Seg -> String
+mkAssign seg = let
+    
 
 
 pt = p2 (1,1) :: P2 Double
